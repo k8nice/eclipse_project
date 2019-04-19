af78cc9353e70159a1b3a943957143be344bf96a
nice <15002450373@163.com>
2019-04-19 09:23:57 +0800

    增加图片上传功能

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/impl/PictureServiceImpl.java
@@ -0,0 +1,13 @@
+package com.taotao.service.impl;
+
+import com.taotao.service.PictureService;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Map;
+
+public class PictureServiceImpl implements PictureService {
+    @Override
+    public Map uploadPicture(MultipartFile uploadFile) {
+        return null;
+    }
+}
